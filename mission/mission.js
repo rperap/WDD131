581bbba73ec8bcb0396ae3c498a97c2c9c6eed3c
rperap <rpereap@unicartagena.com>
2025-10-25 23:31:09 -0500

    added changes

--- mission/mission.js
+++ mission/mission.js
@@ -4,7 +4,7 @@ const logo = document.querySelector("#logo");
 function changeTheme() {
   if (themeSelector.value === "dark") {
     document.body.classList.add("dark");
-    logo.src = "byui-logo_white.webp"; // <-- Make sure you have this image
+    logo.src = "byui-logo_white.webp"; 
   } else {
     document.body.classList.remove("dark");
     logo.src = "byui-logo_blue.webp";
